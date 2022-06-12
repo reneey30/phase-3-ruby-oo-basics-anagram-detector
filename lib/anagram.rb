dc0ed00d4e414620e1ceb1f14a0c897706c74c7a
Irene Njeru <irenenjeru8@gmail.com>
2022-06-12 22:03:24 +0300

    Task completed

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
 # Your code goes here!
+class Anagram
+
+    def initialize(word)
+        @word =word
+    end
+
+def match(array)
+    @array = array
+    new_word = word_sort(@word)
+    p word = @array.filter {
+      |item| word_sort(item) == new_word
+    }
+  end
+
+  def word_sort word
+    word.split("").sort
+  end
+
+end
